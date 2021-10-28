12c2a9c332f0a7a41d42feab4bdcf40bd8b6a092
assomatic <90207584+assomatic@users.noreply.github.com>
2021-10-28 10:35:12 +0200

    initial

---
+++ build/precache-manifest.9456f9614758da9e33e19ec740175fd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882b80656b74e606f49b5e120ca1a213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db525835a7141b58764f",
+    "url": "/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db525835a7141b58764f",
+    "url": "/static/js/main.6610c193.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/static/media/logo.9a3d2645.svg"
+  }
+]);
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem
+        voluptatem voluptate incidunt officia suscipit libero rem consequuntur
+        saepe itaque corrupti impedit aperiam blanditiis, fuga excepturi
+        accusamus magni voluptatibus fugiat laborum nostrum quia adipisci illo
+        doloribus, perspiciatis obcaecati? Nemo, error beatae.
+      </p>
+    </section>
+  );
+};
+
+export default About;
